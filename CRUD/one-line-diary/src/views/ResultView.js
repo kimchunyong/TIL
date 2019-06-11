b7c34f24cf949f41e257c22a6ed64c5e0fb960e6
kimchunyong <lovemy58511@gmail.com>
2019-06-11 19:23:13 +0900

    [add] data list view render

--- CRUD/one-line-diary/src/views/ResultView.js
+++ CRUD/one-line-diary/src/views/ResultView.js
@@ -8,12 +8,26 @@ ResultView.setup = function(el){
     this.init(el);
 }
 
+ResultView.messages = {
+    NO_RESULT: '첫 내용을 입력해주세요',
+}
+
 ResultView.render = function(data = []) {
-    this.el.innerHTML = data.length ? this.getInputResultHTML(data) : '첫 내용을 입력해주세요';
+    this.el.innerHTML = data.length ? this.getInputResultHTML(data) : this.messages.NO_RESULT;
 }
 
 ResultView.getInputResultHTML = function(data){
+    return data.reduce((html, item, idx)=>{
+        html += this.getInputReslutList(item);
+        return html;
+    },'<ul class="write__menu">') + '</ul>'
     debugger;
+}
+
+ResultView.getInputReslutList = function(item){
+    return `<li class="write__item">
+        ${item.text}
+    </li>`
 }
 
 export default ResultView;
